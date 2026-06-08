8f6af8bdb2bdc7e7c2a07b07f5827d96aa34040b
sagun4 <sagunpaudel12@gmail.com>
2026-06-08 09:21:36 +0545

    Add dual trailing slash and non-trailing slash URL routing support in Django

--- backend/djangobnb_backend/properties/urls.py
+++ backend/djangobnb_backend/properties/urls.py
@@ -2,10 +2,15 @@ from django.urls import path
 from . import api
 
 urlpatterns = [
- path('', api.property_list, name='property-list'),
- path('create/', api.create_property, name='create-property'),
- path('<uuid:property_id>/', api.property_detail, name='property-detail'),
- path('<uuid:pk>/book/', api.book_property, name='book-property'),
- path('<uuid:property_id>/reservations/', api.property_reservations, name='property-reservations'),
- path('<uuid:pk>/toggle_favourite/', api.toggle_favourite, name='toggle-favourite'),
+    path('', api.property_list, name='property-list'),
+    path('create/', api.create_property, name='create-property'),
+    path('create', api.create_property),
+    path('<uuid:property_id>/', api.property_detail, name='property-detail'),
+    path('<uuid:property_id>', api.property_detail),
+    path('<uuid:pk>/book/', api.book_property, name='book-property'),
+    path('<uuid:pk>/book', api.book_property),
+    path('<uuid:property_id>/reservations/', api.property_reservations, name='property-reservations'),
+    path('<uuid:property_id>/reservations', api.property_reservations),
+    path('<uuid:pk>/toggle_favourite/', api.toggle_favourite, name='toggle-favourite'),
+    path('<uuid:pk>/toggle_favourite', api.toggle_favourite),
 ]
--- backend/djangobnb_backend/useraccount/urls.py
+++ backend/djangobnb_backend/useraccount/urls.py
@@ -8,9 +8,15 @@ from .views import CustomRegisterView
 
 urlpatterns = [
     path('register/', CustomRegisterView.as_view(), name='rest_register'),
+    path('register', CustomRegisterView.as_view()),
     path('login/', LoginView.as_view(), name='rest_login'),
+    path('login', LoginView.as_view()),
     path('logout/', LogoutView.as_view(), name='rest_logout'),
+    path('logout', LogoutView.as_view()),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/refresh', TokenRefreshView.as_view()),
     path('myreservations/', api.reservations_list, name='reservations-list'),
+    path('myreservations', api.reservations_list),
     path('<uuid:pk>/', api.landlord_detail, name='landlord-detail'),
+    path('<uuid:pk>', api.landlord_detail),
 ]
